d63e467be1a519902795f829aec172485dc11827
root <root@architect23-25507-2.asia-northeast3-a.c.architect-certification-289902.internal>
2023-09-06 05:40:20 +0000

    updated

--- apitest-testrunservice/src/main/java/com/samsungsds/eshop/cart/CartController.java
+++ apitest-testrunservice/src/main/java/com/samsungsds/eshop/cart/CartController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@RequestMapping(value = "/api/carts")
+@RequestMapping(value = "/api/testruns")
 public class CartController {
     private final CartService cartService;
 

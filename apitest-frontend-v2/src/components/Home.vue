c754f7e8c04ac417d10e77b632440454a5c98c6d
root <root@architect23-25507.asia-northeast3-a.c.architect-certification-289902.internal>
2023-09-12 07:25:33 +0000

    updated

--- apitest-frontend-v2/src/components/Home.vue
+++ apitest-frontend-v2/src/components/Home.vue
@@ -3,7 +3,7 @@
     <b-jumbotron class="text-center mb-0" :style="{backgroundColor: bannercolor}">
       <b-container>
         <h1 class="jumbotron-heading">
-            ★ Accelerating API Quality Through Testing ★
+            ★★ Accelerating API Quality Through Testing ★★
         </h1>
         <p class="lead text-muted">
             Automate and fit API testing into your team’s continuous delivery pipeline with the next generation tool built for validation of REST, SOAP, microservices, and other back-end services.
